f301fe18d843652b6ac38e3454cffbac3dffbd11
leshka290 <lleesshhkkaa00@gmail.com>
2023-08-25 11:37:05 +0300

    first commit

---
+++ src/main/java/com/skyteam/shopping_area/ShoppingAreaApplication.java
@@ -0,0 +1,13 @@
+package com.skyteam.shopping_area;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingAreaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppingAreaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/skyteam/shopping_area/ShoppingAreaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skyteam.shopping_area;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingAreaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
