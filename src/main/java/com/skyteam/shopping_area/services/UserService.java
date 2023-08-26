7e884a51f4835a0cc4ed5849979b5a8f58a58476
leshka290 <lleesshhkkaa00@gmail.com>
2023-08-26 10:22:45 +0300

    add Image, ImageService, NewPasswordDto, Exceptions, AppConfiguration

--- src/main/java/com/skyteam/shopping_area/services/UserService.java
+++ src/main/java/com/skyteam/shopping_area/services/UserService.java
@@ -1,9 +1,25 @@
 package com.skyteam.shopping_area.services;
 
+import com.skyteam.shopping_area.dto.NewPasswordDto;
+import com.skyteam.shopping_area.dto.UserDto;
+import com.skyteam.shopping_area.models.User;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.multipart.MultipartFile;
+
 /**
- *Методы для работы с классом User
+ * Методы для работы с классом User
+ *
  * @author leshka290
  */
 public interface UserService {
-    
+
+    boolean setPassword(NewPasswordDto newPasswordDto);
+
+    UserDto getUser(Authentication auth);
+
+    UserDto updateUser(UserDto userDto);
+
+    void updateUserImage(MultipartFile file);
+
+    User findAuthUser();
 }
