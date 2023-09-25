47b12b80c6ed1ac102218aeb717155994719770a
leshka <lleesshhkkaa00@gmail.com>
2023-09-25 22:14:35 +0300

    refactoring UserDetailsManagerCustom

--- src/main/java/com/skyteam/shopping_area/service/impl/UserDetailsManagerCustom.java
+++ src/main/java/com/skyteam/shopping_area/service/impl/UserDetailsManagerCustom.java
@@ -1,6 +1,9 @@
 package com.skyteam.shopping_area.service.impl;
 
+import com.skyteam.shopping_area.dto.RegisterDto;
+import com.skyteam.shopping_area.dto.Role;
 import com.skyteam.shopping_area.dto.UserDetailsCustom;
+import com.skyteam.shopping_area.exception.IncorrectUsernameException;
 import com.skyteam.shopping_area.model.User;
 import com.skyteam.shopping_area.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -8,6 +11,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -23,19 +27,33 @@ import java.util.Optional;
 public class UserDetailsManagerCustom implements UserDetailsService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder encoder;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         Optional<User> optionalUser = userRepository.findUserByEmailIgnoreCase(username);
-        System.out.println(userRepository.findUserByEmailIgnoreCase(username));
         if (optionalUser.isPresent()) {
             User user = optionalUser.get();
-            user.setUsername(user.getEmail());
-            System.out.println(user);
-            System.out.println(new UserDetailsCustom(user).getUsername());
             return new UserDetailsCustom(user);
         } else {
             throw new UsernameNotFoundException("User not found with username: " + username);
         }
+    }
+
+    public void createUser(RegisterDto register) {
+        if (userRepository.findUserByEmailIgnoreCase(register.getUsername()).isPresent()) {
+            throw new IncorrectUsernameException();
+        }
+
+        User user = new User();
+        user.setEmail(register.getUsername());
+        user.setPassword(encoder.encode(register.getPassword()));
+        user.setRole(Role.USER);
+        user.setFirstName(register.getFirstName());
+        user.setLastName(register.getLastName());
+        user.setPhone(register.getPhone());
+        log.info("Сохранение в БД user");
+        log.info(String.valueOf(user));
+        userRepository.save(user);
     }
 }
